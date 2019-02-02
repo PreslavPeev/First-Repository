f341404d2ea020543160f66da4c5d4ba35ee9867
PreslavPeev <47269309+PreslavPeev@users.noreply.github.com>
2019-02-02 10:42:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var i = 1;
+            while(i<=n)
+            {
+                Console.WriteLine(i);
+                i = i * 2 + 1;
+            }
+
+        }
+    }
+}
